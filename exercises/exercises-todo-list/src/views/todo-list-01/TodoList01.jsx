99751bb9528c33bf12da895f7148ca4afee91801
Andres Giraldo Arenas <andresganc@gmail.com>
2022-12-27 17:49:34 -0500

    Mod: Todo List 01

--- exercises/exercises-todo-list/src/views/todo-list-01/TodoList01.jsx
+++ exercises/exercises-todo-list/src/views/todo-list-01/TodoList01.jsx
@@ -1,37 +1,75 @@
 
 import { useState, useEffect } from 'react'
-import { Article, Div, H5, Input, Button } from 'nc-styles-react'
+import Icon from '@mdi/react'
+import { Article, Div, Ul, Li, H4, P, Input, Button } from 'nc-styles-react'
+
+// Icons
+import { mdiDeleteForever } from '@mdi/js'
 
 const TodoList01 = () => {
-  const [stateList, setStateList] = useState([])
+  const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState([])
+  const [statusItem, setStatusItem] = useState(false)
 
   useEffect(() => {
-    console.log(stateList)
-  }, [stateList])
+    console.log(items)
+  }, [items, statusItem])
 
-  const handleChange = (e) => {
-    setStateList(e.target.value)
+  const handleAddItem = (e) => {
+    e.preventDefault()
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+      status: false
+    }
+
+    setItems(arrayTemp => [...arrayTemp, item])
+    setNewItem('')
   }
 
-  const handleAddTodo = () => {
-    e.preventDefault()
-    setStateList([...stateList])
+  const handleDeleteItem = (id) => {
+    const newArray = items.filter(x => x.id !== id)
+    setItems(newArray)
+  }
+
+  const handleStatusItem = (id) => {
+    // (items[id].status = true
+    /*
+    items.map(function(id) {
+      if (x.id === id) {
+        x.status = true
+        setStatusItem(false)
+      }
+    })
+    */
   }
 
   return (
     <>
-        <Article>
-            <Div>
-                <H5>TODO LIST 01</H5>
-            </Div>
-            <Div>
-                <Input type="text" onChange={handleChange} />
-                <Button onClick={handleAddTodo}>Add</Button>
-            </Div>
-            <Div>
-
-            </Div>
-        </Article>
+      <Article jcCenter padding='1rem' bgColorBlue300 width='20rem' borderRadius='1rem' >
+        <Div>
+          <Input type='text' onChange={e => setNewItem(e.target.value)} value={newItem}/>
+          <Button onClick={handleAddItem}>Add</Button>
+        </Div>
+        <Div>
+          <Ul>
+            {
+              items.map(x => {
+                return (
+                  <Div key={x.id} display='flex' aiCenter marginY='.5rem'>
+                    <Input type='checkbox' onClick={() => handleStatusItem(x.id)} />
+                    <Li><P colorWhite>{x.value}</P></Li>
+                    <Button onClick={() => handleDeleteItem(x.id)} button1 marginL='1rem' bgColorRed500>
+                      <Icon path={mdiDeleteForever} size='1rem' color='#FFFFFF' />
+                    </Button>
+                  </Div>
+                )
+              })
+            }
+          </Ul>
+        </Div>
+      </Article>
     </>
   )
 }
