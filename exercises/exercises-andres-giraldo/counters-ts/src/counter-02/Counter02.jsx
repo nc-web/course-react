fcb5fc53fb5856a59131e6a921d69eef7a3505aa
Andres Giraldo <andresganc@gmail.com>
2023-12-10 14:27:29 -0500

    Exercises Andres Counter 02

---
+++ exercises/exercises-andres-giraldo/counters-ts/src/counter-02/Counter02.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react"
+
+export default function Counter02() {
+
+    const [count, setCount] = useState(0)
+
+    return(
+        <>
+            <h2>Counter 03</h2>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+                </button>
+                <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+        </>
+    )
+}
