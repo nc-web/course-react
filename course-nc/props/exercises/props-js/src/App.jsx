1ec963e0084a7e65e347e4315a05983ba407131f
Andres Giraldo <andresganc@gmail.com>
2024-01-16 18:38:45 -0500

    Course NC: Props: Exercises Props Js

--- course-nc/props/exercises/props-js/src/App.jsx
+++ course-nc/props/exercises/props-js/src/App.jsx
@@ -1,11 +1,20 @@
 
 import DefaultProps from "./default-props/DefaultProps"
+import Props from "./props/Props"
 
 function App() {
 
   return (
     <>
+      <h3>Deafult Props</h3>
+      <h4>Valores por defecto</h4>
       <DefaultProps/>
+      <hr/>
+      <h4>Volores por defecto modificados</h4>
+      <DefaultProps width={120} height={80} color='Blue'/>
+      <hr/>
+      <h3>Props</h3>
+      <Props text='Prop de Texto' x={45} y={38} />
     </>
   )
 }
---
+++ course-nc/props/exercises/props-js/src/props/Props.jsx
@@ -0,0 +1,10 @@
+
+export default function DefaultProps(props) {
+    return(
+        <div>
+            <p>Texto: {props.text}</p>
+            <p>X: {props.x}</p>
+            <p>Y: {props.y}</p>
+        </div>
+    ) 
+}
