db6a73088d42f6499608e575ac5bcd04e962e044
Andres Giraldo <andresganc@gmail.com>
2024-04-11 12:40:12 -0500

    Mod: Todo List TS: Todo List 02 with useRef

--- exercises/01-basic/exercises-todo-list/todo-list-ts/src/views/todos/todo-list-02/TodoList02.tsx
+++ exercises/01-basic/exercises-todo-list/todo-list-ts/src/views/todos/todo-list-02/TodoList02.tsx
@@ -16,24 +16,20 @@ type TypeTodoList = ITodoList[]
 
 export default function TodoList01() {
 
-  const refInputTask = useRef()
+  const refInputTask = useRef<HTMLInputElement>()
   
-  const [stateNewItem, setStateNewItem] = useState('')
+  const [stateNewItem, setStateNewItem] = useState<string | undefined> ('')
   const [stateItems, setStateItems] = useState<TypeTodoList>([])
   const [stateStatusItem, setStateStatusItem] = useState(Boolean)
 
   useEffect(() => {
-    console.log(stateNewItem)
-    console.log(stateItems)
+    // console.log(stateNewItem)
+    // console.log(stateItems)
   }, [stateNewItem, stateItems, stateStatusItem])
 
 
   const handleOnChangeInputTask = () => {
-    
-    // const inputTask = e.target as HTMLInputElement
-
-    setStateNewItem(refInputTask.current.value)
-
+    setStateNewItem(refInputTask.current?.value)
   }
 
 
@@ -60,31 +56,32 @@ export default function TodoList01() {
 
   const checkItem = (id: number) => {
     
-    // e.preventDefault()
-    // const inputTask = e.target as HTMLInputElement
-    
-    // const taskFound = stateItems.find(x => x.id === id)
+    const taskFound = stateItems.find(x => x.id === id)
     // console.log(taskFound)
 
-    // if (taskFound) {
-    //   if (taskFound?.status === false) {
-    //     taskFound.status = true
-    //   } else {
-    //     taskFound.status = false
-    //   }
-    // }
-
-    stateItems.map(x => {
-      if (x.id === id) {
-        if (x.status === false) {
-              x.status = true
-              setStateStatusItem(true)
-            } else {
-              x.status = false
-              setStateStatusItem(false)
-            }             
+    if (taskFound) {
+      if (taskFound?.status === false) {
+        taskFound.status = true
+        setStateStatusItem(true)
+        console.log(taskFound)
+      } else if (taskFound?.status === true){
+        taskFound.status = false
+        setStateStatusItem(false)
+        console.log(taskFound)
       }
-    })
+    }
+
+    // stateItems.map(x => {
+    //   if (x.id === id) {
+    //     if (x.status === false) {
+    //           x.status = true
+    //           setStateStatusItem(true)
+    //         } else {
+    //           x.status = false
+    //           setStateStatusItem(false)
+    //         }             
+    //   }
+    // })
 
   }
 
@@ -114,11 +111,11 @@ export default function TodoList01() {
                 return (
                   <div className={styles.tl1__tasks_items} key={x.id}>
                     
-                    { x.status === true ? 
+                    { x.status === false ? 
                       (
-                        <svg className={styles.tl1__tasks_items_svg_point_true} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>check-decagram</title><path d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z" /></svg>
+                        <svg className={styles.tl1__tasks_items_svg_point_false} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points-circle</title><path d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z" /></svg> 
                       ) : (
-                        <svg className={styles.tl1__tasks_items_svg_point_false} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points-circle</title><path d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z" /></svg>
+                        <svg className={styles.tl1__tasks_items_svg_point_true} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>check-decagram</title><path d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z" /></svg>
                       )
                     }
                     
